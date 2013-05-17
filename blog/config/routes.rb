79732b1729c675bdd8bd8c77464a96f7fb3e4feb
fclausen <fclausen@ubuntu-1.(none)>
2013-05-17 10:54:48 +0200

    Created first app with scaffolding

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,7 @@
 Blog::Application.routes.draw do
+  resources :posts
+
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
