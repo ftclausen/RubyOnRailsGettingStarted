7df79523e69a7f5ce8692329334d0941195f057d
fclausen <fclausen@ubuntu-1.(none)>
2013-05-17 10:40:15 +0200

    Added blog learning application

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'eb074d916c57132e654ec82dbb8bf6b6ca6402a0fd2e23561f78e00780b06a5fbfd112b4c7b1db021b76ad9c7e8a03e1a1958390ee3cc1ba06a4e3df667ac3a1'
